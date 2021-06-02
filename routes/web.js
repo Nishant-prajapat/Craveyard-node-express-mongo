d0525b25a9febc73b5825092e0ca74aed9cac7cd
Nishant Prajapati <65334557+Nishant-prajapat@users.noreply.github.com>
2021-06-02 09:20:41 +0530

    Signed-off-by: Nishant Prajapati <semi final changes>

--- routes/web.js
+++ routes/web.js
@@ -23,6 +23,7 @@ function initRoutes(app) {
 
     // Customer routes
     app.post('/orders', auth, orderController().store)
+    //app.post('/np', auth, orderController().store)
     app.get('/customer/orders', auth, orderController().index)
     app.get('/customer/orders/:id', auth, orderController().show)
 
