7805bd3e7b145ffa6e6f1da586a668afbc928478
Nishant Prajapati <65334557+Nishant-prajapat@users.noreply.github.com>
2021-09-07 16:48:18 +0530

    Update server.js

--- server.js
+++ server.js
@@ -11,7 +11,6 @@ const flash = require('express-flash')
 const MongoDbStore = require('connect-mongo')(session)
 const passport = require('passport')
 const Emitter = require('events')
-
 // Database connection
 mongoose.connect(process.env.MONGO_CONNECTION_URL, { useNewUrlParser: true, useCreateIndex:true, useUnifiedTopology: true, useFindAndModify : true });
 const connection = mongoose.connection;
@@ -20,18 +19,14 @@ connection.once('open', () => {
 }).catch(err => {
     console.log('Connection failed...')
 });
-
-
 // Session store
 let mongoStore = new MongoDbStore({
                 mongooseConnection: connection,
                 collection: 'sessions'
             })
-
 // Event emitter
 const eventEmitter = new Emitter()
 app.set('eventEmitter', eventEmitter)
-
 // Session config
 app.use(session({
     secret: process.env.COOKIE_SECRET,
@@ -40,19 +35,16 @@ app.use(session({
     saveUninitialized: false,
     cookie: { maxAge: 1000 * 60 * 60 * 24 } // 24 hour
 }))
-
 // Passport config
 const passportInit = require('./app/config/passport')
 passportInit(passport)
 app.use(passport.initialize())
 app.use(passport.session())
-
 app.use(flash())
 // Assets
 app.use(express.static('public'))
 app.use(express.urlencoded({ extended: false }))
 app.use(express.json())
-
 // Global middleware
 app.use((req, res, next) => {
     res.locals.session = req.session
@@ -68,13 +60,10 @@ require('./routes/web')(app)
 app.use((req, res) => {
     res.status(404).render('errors/404')
 })
-
 const server = app.listen(PORT , () => {
             console.log(`Listening on port ${PORT}`)
         })
-
 // Socket
-
 const io = require('socket.io')(server)
 io.on('connection', (socket) => {
       // Join
@@ -82,12 +71,9 @@ io.on('connection', (socket) => {
         socket.join(orderId)
       })
 })
-
 eventEmitter.on('orderUpdated', (data) => {
     io.to(`order_${data.id}`).emit('orderUpdated', data)
 })
-
 eventEmitter.on('orderPlaced', (data) => {
     io.to('adminRoom').emit('orderPlaced', data)
 })
-
